4d36e21d426948780424a3ee6c130fb789606ec9
Melissa Geels <spansjh@gmail.com>
2020-06-01 01:49:30 +0200

    Added --folder, --recursive, and --compact options to parseflp

--- src/parseflp/main.cpp
+++ src/parseflp/main.cpp
@@ -1,9 +1,30 @@
 #include <flp/flp.h>
+#include <flp/Folder.h>
 
 #include <cctype>
 
-static void ReportInfo(FlpFile& flp)
+static void ReportInfo(const FlpFile& flp, bool compact)
 {
+	if (compact) {
+		s2::string strLine = flp.Filename();
+
+		strLine += s2::strprintf(" / %g BPM", flp.GetBPM());
+
+		auto& meta = flp.GetMetadata();
+		if (meta.m_genre != "") {
+			strLine += s2::strprintf(" / %s", meta.m_genre.c_str());
+		}
+		if (meta.m_title != "") {
+			strLine += s2::strprintf(" / %s", meta.m_title.c_str());
+		}
+		if (meta.m_author != "") {
+			strLine += s2::strprintf(" / %s", meta.m_author.c_str());
+		}
+
+		printf("%s\n", strLine.c_str());
+		return;
+	}
+
 	printf("%s:\n", flp.Filename().c_str());
 
 	if (!flp.IsValid()) {
@@ -57,8 +78,10 @@ static s2::string EncodeLicensee(s2::string name)
 
 static void PrintUsage()
 {
-	printf("Usage: parseflp <path>\n");
+	printf("Usage: parseflp [options] <path>\n");
 	printf("Usage: parseflp --encode-licensee <name>\n");
+	printf("\nOptions:\n");
+	printf("  --folder, --recursive, --compact\n");
 }
 
 int main(int argc, char* argv[])
@@ -68,18 +91,45 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	if (!strcmp(argv[1], "--encode-licensee")) {
-		if (argc < 3) {
-			PrintUsage();
-			return 1;
-		}
+	bool reportCompact = false;
+	bool readFolder = false;
+	bool readFolderRecursive = false;
+	s2::string strInput;
 
-		s2::string name = EncodeLicensee(argv[2]);
-		printf("%s\n", name.c_str());
-		return 0;
+	for (int i = 1; i < argc; i++) {
+		s2::string strArg = argv[i];
+
+		if (strArg.len() > 0 && strArg[0] == '-') {
+			if (strArg == "--encode-licensee" && i + 1 < argc) {
+				s2::string name = EncodeLicensee(argv[++i]);
+				printf("%s\n", name.c_str());
+				return 0;
+
+			} else if (strArg == "--folder") {
+				readFolder = true;
+
+			} else if (strArg == "--recursive") {
+				readFolderRecursive = true;
+
+			} else if (strArg == "--compact") {
+				reportCompact = true;
+			}
+		} else {
+			strInput = strArg;
+		}
 	}
 
-	FlpFile flp(argv[1]);
-	ReportInfo(flp);
+	if (readFolder) {
+		FolderIndex folder(strInput, readFolderRecursive);
+		for (int i = 0; i < folder.GetFileCount(); i++) {
+			s2::string strPath = folder.GetFilePath(i);
+			if (strPath.endswith(".flp")) {
+				ReportInfo(FlpFile(strPath), reportCompact);
+			}
+		}
+	} else {
+		ReportInfo(FlpFile(strInput), reportCompact);
+	}
+
 	return 0;
 }
